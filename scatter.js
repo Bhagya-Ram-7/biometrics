76ed5caa2ad9ca0da884592d1318015d53fa3663
Bhagya Ram <bhagya.sram@gmail.com>
2025-02-17 15:20:33 -0800

    included additional biometric

--- scatter.js
+++ scatter.js
@@ -23,7 +23,7 @@ const biometricDropdown = d3.select("#biometricDropdown");
 const examDropdown = d3.select("#examDropdown");
 
 function populateDropdowns() {
-    const biometricOptions = ["temp", "EDA", "heart_rate"];
+    const biometricOptions = ["Temperature", "Electradermal Activity", "Heart Rate", "IBI"];
     const examOptions = ["Midterm 1", "Midterm 2", "Final"];
     biometricDropdown.selectAll("option").remove();
     examDropdown.selectAll("option").remove();
@@ -39,7 +39,6 @@ function updateChart(data, selectedBiometric, selectedExam) {
 
     const xExtent = d3.extent(data, d => d.biometric[selectedBiometric][selectedExam]);
     const xPadding = (xExtent[1] - xExtent[0]) * 0.1; 
-    //xScale.domain([xExtent[0] - xPadding, xExtent[1] + xPadding]);
 
     const xScale = d3.scaleLinear()
         .domain([xExtent[0] - xPadding, xExtent[1] + xPadding])
@@ -49,8 +48,7 @@ function updateChart(data, selectedBiometric, selectedExam) {
         .domain([0, 100])  
         .range([350, 50]);
 
-    // Plot Data Points
-    svgGroup.selectAll("circle")
+        svgGroup.selectAll("circle")
         .data(data)
         .enter()
         .append("circle")
@@ -59,20 +57,71 @@ function updateChart(data, selectedBiometric, selectedExam) {
         .attr("r", 6)
         .attr("fill", "blue")
         .on("mouseover", (event, d) => {
+            d3.select(this)
+                .transition()
+                .duration(200)
+                .attr("r", 10);
             d3.select("#tooltip").style("display", "block")
                 .html(`${d.student}: ${d.biometric[selectedBiometric][selectedExam]}, Grade: ${d.grades[selectedExam]}`)
                 .style("left", (event.pageX + 10) + "px")
                 .style("top", (event.pageY - 10) + "px");
         })
-        .on("mouseout", () => d3.select("#tooltip").style("display", "none"));
-
-    svgGroup.append("g")
+        .on("mouseout", function() {
+            d3.select(this)
+                .transition()
+                .duration(200)
+                .attr("r", 6);
+                d3.select("#tooltip").style("display", "none");
+        });
+        svgGroup.append("g")
         .attr("transform", "translate(0,350)")
         .call(d3.axisBottom(xScale));
 
     svgGroup.append("g")
         .attr("transform", "translate(50,0)")
         .call(d3.axisLeft(yScale));
+
+
+    svgGroup.append("text")
+        .attr("class", "x axis-label")
+        .attr("text-anchor", "middle")
+        .attr("x", width / 2)
+        .attr("y", height + margin.bottom )
+        .text(selectedBiometric);
+
+    svgGroup.append("text")
+        .attr("class", "y axis-label")
+        .attr("text-anchor", "middle")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -height / 2)
+        .attr("y", -margin.left + 60)
+        .text(selectedExam);
+
+    const xValues = data.map(d => d.biometric[selectedBiometric][selectedExam]);
+    const yValues = data.map(d => d.grades[selectedExam]);
+    function linearRegression(x, y) {
+        const n = x.length;
+        const sumX = d3.sum(x);
+        const sumY = d3.sum(y);
+        const sumXY = d3.sum(x.map((xi, i) => xi * y[i]));
+        const sumX2 = d3.sum(x.map(xi => xi * xi));
+        const slope = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX * sumX);
+        const intercept = (sumY - slope * sumX) / n;
+        return { slope, intercept };}
+    const { slope, intercept } = linearRegression(xValues, yValues);
+
+    const x1 = xExtent[0] - xPadding;
+    const y1 = slope * x1 + intercept;
+
+    const x2 = xExtent[1] + xPadding;
+    const y2 = slope * x2 + intercept;
+    svgGroup.append("line")
+        .attr("x1", xScale(x1))
+        .attr("y1", yScale(y1))
+        .attr("x2", xScale(x2))
+        .attr("y2", yScale(y2))
+        .attr("stroke", "red")
+        .attr("stroke-width", 2);
 }
 
 const tooltip = d3.select("body").append("div")
